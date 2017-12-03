6937ecd97317e5285ecb54a43e39b648a0c2d371
avf2001 <avf2001@yandex.ru>
2017-12-03 20:20:35 +0300

    Empty project created

---
+++ RunningApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
