f2017d6427e5c1dd810b7772b9af797a64d2c6a1
avf2001 <avf2001@yandex.ru>
2018-02-12 22:28:46 +0300

    Добавлено решение задачи на массивы Maximum Subarray

---
+++ ArrayQuestionsUnitTests/Task07MaximumSubarrayTest.cs
@@ -0,0 +1,21 @@
+﻿using ArraysQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayQuestionsUnitTests
+{
+    [TestClass]
+    public class Task07MaximumSubarrayTest
+    {
+        [TestMethod]        
+        public void Test()
+        {
+            var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var result = Task07MaximumSubarray.Do(nums);
+
+            Assert.IsTrue(result == 6);
+        }
+    }
+}
