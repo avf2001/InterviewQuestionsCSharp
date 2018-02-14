693beebf654f9aa26fe075f7fd5e2a51620e2223
avf2001 <avf2001@yandex.ru>
2018-02-14 22:16:04 +0300

    В раздел ArrayQuestions добавлено решение задачи Merge Sorted Arrays

---
+++ ArrayQuestionsUnitTests/Task09MergeSortedArrayTest.cs
@@ -0,0 +1,22 @@
+﻿using ArraysQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayQuestionsUnitTests
+{
+    [TestClass]
+    public class Task09MergeSortedArrayTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var nums1 = new int[] { 1, 2, 3, 0, 0, 0, 0 };
+            var n = 3;
+            var nums2 = new int[] { 1, 2, 3, 4};
+            var m = 4;
+            Task09MergeSortedArray.Do(nums1, 3, nums2, 4);
+        }
+    }
+}
