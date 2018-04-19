d0a9a32fd21f1eb5d0c15ce526e9de20a68ce3de
avf2001 <avf2001@yandex.ru>
2018-04-19 20:07:24 +0300

    Добавлено решение задачи на строки Is Unique

---
+++ StringQuestions/Task01IsUnique.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringQuestions
+{
+    /*
+     * Implement an algorithm to determine if a string has all unique characters.
+     * What if you cannot use additional data structures?
+     */
+    public class Task01IsUnique
+    {
+        public static bool Do1(string str)
+        {
+            if (str.Length > 128) return false;
+            
+            bool[] char_set = new bool[128];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int val = str[i];
+
+                if (char_set[val])
+                {   //Already found this char in string
+                    return false;
+                }
+
+                char_set[val] = true;
+            }
+
+            return true;
+        }
+    }
+}
