477b872705ec1b7cb48e9c548e58aaeeef19514a
avf2001 <avf2001@yandex.ru>
2018-07-19 21:58:56 +0300

    Добавлено условие задачи на массивы Search in Rotated Sorted Array

---
+++ ArraysQuestions/Task26SearchInRotatedArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysQuestions
+{
+    /*
+     * Search in Rotated Sorted Array
+     * 
+     * Given a sorted array of n integers that has been rotated an unknown number of times,
+     * write code to find an element in the array. You may assume that the array was originally sorted in increasing order.
+     * EXAMPLE
+     * Input: findSin{lS, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14}
+     * Output: 8 (the index of 5 in the array)
+     * 
+     * Cracking the Coding Interview, p. 150, 10.3
+     * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+     */
+    class Task26SearchInRotatedArray
+    {
+    }
+}
--- StringQuestions/Task07GroupAnagrams.cs
+++ StringQuestions/Task07GroupAnagrams.cs
@@ -10,6 +10,7 @@ namespace StringQuestions
      * Write a method to sort an array of strings so that all the anagrams are next to each other.
      * 
      * Cracking the Coding Interview, p. 396, 10.2
+     * https://leetcode.com/problems/group-anagrams/description/
      */
     class Task07GroupAnagrams
     {
