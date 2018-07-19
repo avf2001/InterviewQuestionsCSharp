e6559c064387a65553d92206720448bd6533bc64
avf2001 <avf2001@yandex.ru>
2018-07-19 21:49:23 +0300

    Добавлено условие задачи на строки Group Anagrams

---
+++ StringQuestions/Task07GroupAnagrams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringQuestions
+{
+    /*
+     * Group Anagrams
+     * 
+     * Write a method to sort an array of strings so that all the anagrams are next to each other.
+     * 
+     * Cracking the Coding Interview, p. 396, 10.2
+     */
+    class Task07GroupAnagrams
+    {
+    }
+}
