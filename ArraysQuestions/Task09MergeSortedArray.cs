3ce5a10be01cc3f4f5e8ac1927c9ae1ad147a74e
avf2001 <avf2001@yandex.ru>
2018-07-19 21:40:15 +0300

    Добавлена задача на рекурсию

--- ArraysQuestions/Task09MergeSortedArray.cs
+++ ArraysQuestions/Task09MergeSortedArray.cs
@@ -6,6 +6,8 @@
      * Note:
      * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
      * The number of elements initialized in nums1 and nums2 are m and n respectively.
+     * 
+     * Cracking the Coding Interview, p. 396, 10.1
      * https://leetcode.com/problems/merge-sorted-array/description/
      */
     public class Task09MergeSortedArray
---
+++ RecursionQuestions/Task01TowerOfHanoi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecursionQuestions
+{
+    public class Task01TowerOfHanoi
+    {
+        /*
+         * Towers of Hanoi
+         * 
+         * In the classic problem of the Towers of Hanoi, 
+         * you have 3 towers and N disks of different sizes which can slide onto any tower.
+         * The puzzle starts with disks sorted in ascending order of size from top to bottom
+         * (i.e., each disk sits on top of an even larger one).
+         * You have the following constraints:
+         * (1) Only one disk can be moved at a time.
+         * (2) A disk is slid off the top of one tower onto another tower.
+         * (3) A disk cannot be placed on top of a smaller disk.
+         * Write a program to move the disks from the first tower to the last using stacks.
+         * 
+         * Cracking the Coding Interview, p. 135, 8.6
+         */
+        public static void Do()
+        {
+        }
+    }
+}
