bb4dda33f130db3a90840a6b5c5c36ac55d79b58
avf2001 <avf2001@yandex.ru>
2018-07-23 23:14:35 +0300

    Добавлена задача на массивы Sparse Search

---
+++ ArraysQuestions/Task28SparseSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysQuestions
+{
+    /*
+     * Sparse Search
+     * 
+     * Given a sorted array of strings that is interspersed with empty strings,
+     * write a method to find the location of a given string.
+     * EXAMPLE
+     * Input: ball,{"at",""}
+     * Output:4
+     * Cracking the Coding Interview, p. 150, 10.5
+     */
+    class Task28SparseSearch
+    {
+    }
+}
