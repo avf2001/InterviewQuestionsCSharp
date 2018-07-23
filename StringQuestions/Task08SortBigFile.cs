fa59de2fbebad07371c71f79083dcdf33fb93590
avf2001 <avf2001@yandex.ru>
2018-07-23 23:17:43 +0300

    Добавлена задача на строки Sort Big File

---
+++ StringQuestions/Task08SortBigFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringQuestions
+{
+    /*
+     * Sort Big File
+     * 
+     * Imagine you have a 20 GB file with one string per line. Explain how you would sort the file.
+     * 
+     * Cracking the Coding Interview, p. 150, 10.6
+     */
+    class Task08SortBigFile
+    {
+    }
+}
