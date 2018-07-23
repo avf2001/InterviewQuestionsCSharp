7cbb284618cccae90f5a5290ef24c47e6c69f788
avf2001 <avf2001@yandex.ru>
2018-07-23 23:10:13 +0300

    Добавлена новая задача на массивы Sorted Search No Size

---
+++ ArraysQuestions/Task27SortedSearchNoSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysQuestions
+{
+    /*
+     * Sorted Search, No Size
+     * 
+     * You are given an array-like data structure Listy which lacks a size method.
+     * It does, however, have an elementAt ( i) method that returns the element at index i in O(1) time.
+     * If i is beyond the bounds of the data structure, it returns -1.
+     * (For this reason, the data structure only supports positive integers.)
+     * Given a Li sty which contains sorted, positive integers, find the index at which an element x occurs.
+     * If x occurs multiple times, you may return any index.
+     * 
+     * Cracking the Coding Interview, p. 150, 10.4
+     */
+    class Task27SortedSearchNoSize
+    {
+    }
+}
