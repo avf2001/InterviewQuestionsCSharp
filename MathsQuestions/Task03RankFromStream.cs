31c72a9999f468e901eb8207fce1af37d1b334f7
avf2001 <avf2001@yandex.ru>
2018-07-24 20:00:15 +0300

    Добавлено описание задачи на числа Rank From Stream

---
+++ MathsQuestions/Task03RankFromStream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathsQuestions
+{
+    /*
+     * Rank from Stream
+     * 
+     * Imagine you are reading in a stream of integers.
+     * Periodically, you wish to be able to look up the rank of a numberx (the number of values less than or equal to x).
+     * Implement the data structures and algorithms to support these operations.
+     * That is, implement the method track ( int x), which is called when each number is generated,
+     * and the method getRankOfNumber(int x), which returns the number of values less than or equal to x (not including x itself).
+     * 
+     * EXAMPLE
+     * Stream (in order of appearance): 5, 1, 4, 4, 5, 9, 7, 13, 3
+     * getRankOfNumber(1) = 0
+     * getRankOfNumber(3) = 1
+     * getRankOfNumber(4) = 3
+     * 
+     * Cracking the Coding Interview, p. 151, 10.10
+     */
+    class Task03RankFromStream
+    {
+    }
+}
