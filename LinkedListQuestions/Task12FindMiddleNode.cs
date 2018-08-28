a3c7379f0837a0d09650a07756defbd35e72faaa
avf2001 <avf2001@yandex.ru>
2018-08-28 23:31:27 +0300

    Добавлен пустой класс Task12FindMiddleNode

---
+++ LinkedListQuestions/Task12FindMiddleNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListQuestions
+{
+    public class Task12FindMiddleNode
+    {
+        public static void Do()
+        {
+        }
+    }
+}
