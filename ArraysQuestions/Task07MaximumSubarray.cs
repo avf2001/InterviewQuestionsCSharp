55b92660a61385697df283808462ed9dea053fbc
avf2001 <avf2001@yandex.ru>
2018-09-13 23:31:35 +0300

    Добавлен комментарий к задаче на массивы Maximum Subarray (Kadane's algorithm)

--- ArraysQuestions/Task07MaximumSubarray.cs
+++ ArraysQuestions/Task07MaximumSubarray.cs
@@ -1,7 +1,23 @@
 ﻿namespace ArraysQuestions
 {
+    /*
+     * Maximum Subarray (Kadane's algorithm)
+     * 
+     * Given an integer array nums, find the contiguous subarray (containing at least one number)
+     * which has the largest sum and return its sum.
+     * 
+     * Example:
+     * Input: [-2,1,-3,4,-1,2,1,-5,4],
+     * Output: 6
+     * Explanation: [4,-1,2,1] has the largest sum = 6.
+     * 
+     * https://leetcode.com/problems/maximum-subarray/description/
+     */
     public class Task07MaximumSubarray
     {
+        /*
+         * https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+         */
         public static int Do(int[] nums)
         {
             int[] dp = new int[nums.Length];
